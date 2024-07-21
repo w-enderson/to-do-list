2159ebefed6bad88991061692762d41212a3876c
wenderson-juvenal <wendersjuv@gmail.com>
2024-07-21 17:47:12 -0300

    classe de membro criada, tela de cadastro de membro, tela de login,  tela home,

---
+++ Member.js
@@ -0,0 +1,42 @@
+const db = require('./db');
+
+class Member {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    };
+
+
+    insert_db(callback) {
+        const insertQuery = `
+            INSERT INTO members (name, email)
+            VALUES (?, ?)
+        `;
+
+        const values = [this.name, this.email];
+        db.connection.query(insertQuery, values, (err, results) => {
+            if (err) {
+                console.error('Erro ao inserir membro:', err);
+                if (callback) callback(err);
+                return;
+            }
+            console.log('membro inserido com sucesso');
+            if (callback) callback(null, results);
+        });
+    }
+
+    delete_db(memberId, callback) {
+        const deleteQuery = 'DELETE FROM members WHERE id = ?';
+        db.connection.query(deleteQuery, [taskId], (err, results) => {
+            if (err) {
+                console.error('Erro ao deletar membro:', err);
+                if (callback) callback(err);
+                return;
+            }
+            console.log('membro removido');
+            if (callback) callback(null, results);
+        });
+    }
+}
+
+module.exports = Member;
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const db = require('./db');
 const Task = require('./Task')
+const Member = require('./Member')
+const session = require('express-session');
+
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +13,12 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(session({
+    secret: 'ODM32IQJE0923JE0QDX02D9J3E33',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 
 db.connect(() => {
     db.createDatabase('todo_list_db', () => {
@@ -27,7 +36,40 @@ db.connect(() => {
 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('login');
+});
+
+app.get('/checkEmail/:email', (req, res) => {
+    const email = req.params.email;
+
+    
+});
+
+app.post('/login', (req, res) => {
+    const { email } = req.body;
+
+    const selectQuery = 'SELECT * FROM members WHERE email = ?';
+    db.connection.query(selectQuery, [email], (err, results) => {
+        if (err) {
+            console.error('Erro ao verificar email:', err);
+            return res.status(500).send('Erro ao verificar email');
+        }
+        
+        if (results.length > 0) {
+            // Se o membro existir, pegue o nome e armazene na sessão
+            const member = results[0]; // O primeiro resultado
+            req.session.user = { username: member.name }; // Armazena o nome do membro na sessão
+            res.redirect('/home'); // Redireciona para o dashboard
+        } else {
+            res.send('Credenciais inválidas'); // Se não encontrar, retorna mensagem de erro
+        }
+    });
+});
+app.get('/home', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect('/'); 
+    }
+    res.render('home', {username: req.session.user.username} ); // Exibe mensagem de boas-vindas
 });
 
 app.get('/tasks', (req, res) => {
@@ -103,6 +145,37 @@ app.post('/deleteTask/:id', (req, res) => {
 
     res.redirect('/tasks');
 });
+
+
+app.get('/members', (req, res) => {
+    const selectQuery = 'SELECT * FROM members';
+    db.connection.query(selectQuery, (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar membros:', err);
+            return res.status(500).send('Erro ao buscar membros');
+        }
+        res.render('memberList', { members: results }); // Renderiza a view memberList.ejs
+    });
+});
+
+app.get('/newMember', (req, res) => {
+    res.render('newMember'); 
+});
+app.post('/newMember', (req, res) => {
+    const { name, email } = req.body;
+
+    const newMember = new Member(name, email);
+    newMember.insert_db((err, result) => {
+        if (err) {
+            console.error('Erro ao inserir membro:', err);
+            return res.status(500).send('Email já existe');
+        }
+        console.log('Membro cadastrado com sucesso:', result);
+        res.redirect('/'); // Redireciona para a lista de membros após o cadastro
+    });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`)
